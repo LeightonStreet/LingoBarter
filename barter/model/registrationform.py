ad0ce8f37c7bffe42ad59fb5c63370b0636d0e8f
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-02-06 00:19:01 -0500

    update registration form

--- barter/model/registrationform.py
+++ barter/model/registrationform.py
@@ -6,7 +6,8 @@ from wtforms import Form, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
     username = StringField('username', validators.DataRequired())
-    email = StringField('email', validators.DataRequired())
+    email = StringField('email', validators.DataRequired(), validators.Email(message='not a valid email'))
+
     password = PasswordField('password', validators.DataRequired(),
                              validators.EqualTo('confirm', message='Passwords must match'))
     confirm = PasswordField('repeat password', validators.DataRequired())
