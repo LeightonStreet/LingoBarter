ea565425c7da2f56ad5d72543427637579e3c090
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-04-09 22:44:33 -0400

    todo

--- lingobarter/modules/accounts/resouces.py
+++ lingobarter/modules/accounts/resouces.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask_restful import Resource
+from lingobarter.utils import get_current_user
+import json
+from bson import json_util
+from flask import request
 
 
-class TodoItem(Resource):
-    """
-    A sample resource class
-    """
-    def get(self, todo_id):
-        return {'task': 'Say "Hello, World!" {id}'.format(id=todo_id)}
+class LoginResource(Resource):
+    def post(self):
+        pass
+
+class LogoutResource(Resource):
+    def post(self):
+        pass
+
+
+class SignupResource(Resource):
+    def post(self):
+        pass
+
+
+class UserResource(Resource):
+    # get user profile
+    def get(self, user_id):
+        current_user = get_current_user()
+        if user_id == current_user.get_id():
+            return json.dumps(current_user, default=json_util.default)
+        else:
+            pass
+
+    def post(self):
+        form = self.form(request.form)
+        if form.validate():
+            user = get_current_user()
+            # todo: update fields
+            user.save()
