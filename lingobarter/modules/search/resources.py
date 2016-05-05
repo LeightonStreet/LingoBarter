70cff0dcd21172e283ab86abb35cb060d9e99bcb
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-05-05 00:52:46 -0400

    add some comments

--- lingobarter/modules/search/resources.py
+++ lingobarter/modules/search/resources.py
@@ -12,15 +12,21 @@ one can extend from another
 """
 searchParser = reqparse.RequestParser()
 searchParser.add_argument('age_range', type=list, required=False)
-searchParser.add_argument('nationality', type=str, required=False)
+searchParser.add_argument('nationality', type=str, required=False) # list of string
 searchParser.add_argument('teach_langs', type=list, required=True)
 searchParser.add_argument('learn_langs', type=list, required=True)
 searchParser.add_argument('has_bio', type=bool, required=False)
 
+# in database: teach_langs i want to teach ..., learn_langs is want to learn
 
 class SearchResource(Resource):
     @auth_token_required
     def post(self):
         # parse arguments
         args = searchParser.parse_args()
+
+        # response: list:
+        # /users/<userid> get
+        # 20 users per page
+        # skip, limit
 
