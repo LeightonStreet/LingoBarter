bdebd188434b7991f603dc1d4e982d9483870a86
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-05-05 21:46:40 -0400

    mongoengine query Q stash. Let me try raw query.....

--- lingobarter/modules/search/resources.py
+++ lingobarter/modules/search/resources.py
@@ -1,32 +1,142 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask_restful import Resource, reqparse
+import json
+from bson import json_util
+
+from flask_restful import Resource
 from flask_security import auth_token_required
+from flask import request
+
+# from mongoengine.queryset.visitor import Q
+
+from lingobarter.utils import get_current_user
+from lingobarter.core.json import render_json
+from ..accounts.models import User, LanguageItem
+
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
-"""
-Parsers
-=======
-one can extend from another
-"""
-searchParser = reqparse.RequestParser()
-searchParser.add_argument('age_range', type=list, required=False)
-searchParser.add_argument('nationality', type=str, required=False) # list of string
-searchParser.add_argument('teach_langs', type=list, required=True)
-searchParser.add_argument('learn_langs', type=list, required=True)
-searchParser.add_argument('has_bio', type=bool, required=False)
+# in database: teach_langs is what I want to teach , and learn_langs is what I want to learn
 
-# in database: teach_langs i want to teach ..., learn_langs is want to learn
 
+# response: list of results of /users/<userid> get
+# 20 users per page (skip, limit)
 class SearchResource(Resource):
+    """
+    Search the database using filter conditions passes in
+    Return a list of user profiles that meet all search conditions
+    We will return results page by page, 20 records per page
+
+        # 'age_range': list of two elements, representing smallest and largest age acceptable
+        # 'nationaliry': list of strings, representing names of countries acceptable
+        # 'teach_langs': list of language items, language_id representing acceptable teach languages of other users
+        # 'learn_langs': list of language items (language_id represents acceptable learn languages of other users,
+        #                                        level represents acceptable learn languages of other users
+        #                                        default value of level is 0)
+        # 'has_bio': bool, if true, only accept users who have bio in their profile
+    """
     @auth_token_required
     def post(self):
-        # parse arguments
-        args = searchParser.parse_args()
+        # get current user
+        user = get_current_user()
 
-        # response: list:
-        # /users/<userid> get
-        # 20 users per page
-        # skip, limit
+        # we save all filter conditions and corresponding values here
+        # 'age_range': list of two int, representing smallest and largest age acceptable
+        # 'nationality': list of strings, representing numbers of countries acceptable
+        # 'teach_langs': list of LanguageItems, representing acceptable teach languages of other users
+        #                level will be ignored
+        # 'learn_langs': list of LanguageItems, representing acceptable learn languages of other users
+        #                level will not be ignored, set to be 0 if not provided
+        # 'has_bio': a bool value, if true, only accept users who have bio in their profile
+        #            this value will not be False
+        filter_conditions = {}
+
+        # we get filter data from front end
+        filter_data = json.loads(request.data, object_hook=json_util.object_hook)
+
+        # parse filter data
+        if filter_data.get('age_range') is not None:
+            filter_conditions['age_range'] = [int(x) for x in filter_data['age_range']]
+        if filter_data.get('nationality') is not None:
+            filter_conditions['nationality'] = filter_data['nationality']
+        if filter_data.get('teach_langs') is not None:
+            filter_conditions['teach_langs'] = []
+            for language in filter_data['teach_langs']:
+                new_language_item = LanguageItem()
+                new_language_item.language_id = language['language_id']
+                filter_conditions['teach_langs'].append(new_language_item)
+        if filter_data.get('learn_langs') is not None:
+            filter_conditions['learn_langs'] = []
+            for language in filter_data['learn_langs']:
+                new_language_item = LanguageItem()
+                new_language_item.language_id = language['language_id']
+                new_language_item.level = int(language['level']) if language.get('level') is not None else 0
+                filter_conditions['learn_langs'].append(new_language_item)
+        if filter_data.get('has_bio') is not None:
+            if bool(filter_data['has_bio']): # if True, update filter condition
+                filter_conditions['has_bio'] = True
+
+        # query = {'bio': { $exists: True}}
+        query = {'email': 'coco.xin.yang2013@gmail.com'}
+        acceptable_users = User.objects(__raw__=query)
+
+
+        print "*********************"
+        for x in acceptable_users:
+            print x.email
+
+        # # generate filters according to filter_conditions
+        # filters = Q()
+        # # if filter_conditions.get('age_range') is not None:
+        # #     least_birthday = datetime.datetime.now() - relativedelta(years=filter_conditions['age_range'][1])
+        # #     most_birthday = datetime.datetime.now() - relativedelta(years=filter_conditions['age_range'][0])
+        # #     filters = filters & Q(birthday__gte=least_birthday) & Q(birthday__lte=most_birthday)
+        # #     filters = filters & Q(birthday__gte=least_birthday) & Q(birthday__lte=most_birthday)
+        # # if filter_conditions.get('nationality') is not None:
+        # #     filters = filters & Q(nationality__in=filter_conditions['nationality'])
+        # # if filter_conditions.get('teach_langs') is not None:
+        # #     teach_langs_filters = Q()
+        # #     for language in filter_conditions['teach_langs']:
+        # #         teach_langs_filters = (teach_langs_filters | Q(teach_langs__language_id=language.language_id))
+        # #     filters = filters & (teach_langs_filters)
+        # if filter_conditions.get('learn_langs') is not None:
+        #     learn_langs_filters = Q()
+        #     for language in filter_conditions['learn_langs']:
+        #         learn_langs_filters = (learn_langs_filters |
+        #                                Q(language.language_id in User.get_learn_language_id_gte_level(language.level)))
+        #     filters = filters & (learn_langs_filters)
+        # # if filter_conditions.get('has_bio') is not None:
+        # #     filters = filters & Q(bio__ne=None)
+        #
+        #
+        # # Also, result should not include the user himself
+        # filters = filters & Q(username__ne=user.username)
+        # # search
+        # acceptable_users = User.objects(filters)
+        #
+        # print "*********************"
+        # for x in acceptable_users:
+        #     print x.email
+        #     print x.birthday, '\n'
+        #     for lan in x.learn_langs:
+        #         print lan
+
+
+
+
+        return render_json(message='Successfully search users.', status=200)
+
+
+
+
+
+
+
+
+
+
+
+
 
