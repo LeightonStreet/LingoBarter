2a2185067e7cab4143f8b5a5deb8c775bf1657e0
He Li <jet.in.brain@gmail.com>
2016-05-07 14:11:54 -0400

    finish start code for websocket

--- lingobarter/modules/barter/events.py
+++ lingobarter/modules/barter/events.py
@@ -1,8 +1,24 @@
+import functools
+
+from flask.ext.security.decorators import _check_token
+from flask_socketio import disconnect
 from flask_socketio import emit
+
+
+def authenticated_only(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        if not _check_token():
+            disconnect()
+        else:
+            return f(*args, **kwargs)
+
+    return wrapped
 
 
 def register_events(socket_io):
     # get my self
     @socket_io.on('me')
+    @authenticated_only
     def me():
         emit('me', {'message': 'hello!!'})
