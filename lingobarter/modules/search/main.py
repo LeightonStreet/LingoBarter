92fe6211755bb8b838b4cee2afb20abd5eb83601
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-05-13 12:28:43 -0400

    add /content post

--- lingobarter/modules/search/main.py
+++ lingobarter/modules/search/main.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from lingobarter.core.app import LingobarterModule
 from lingobarter.core.api import LingobarterApi
-from .resources import SearchResource
+from .resources import SearchResource, ContentResource
 
 # create our module based on blueprint
 module = LingobarterModule('search', __name__, template_folder='templates')
@@ -10,3 +10,4 @@ api_v1 = LingobarterApi(module, version='v1')
 
 # add restful webservice endpoints
 api_v1.add_lingobarter_resource(SearchResource, '/search')
+api_v1.add_lingobarter_resource(ContentResource, '/content')
